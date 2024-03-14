fb952617e0888aaa484fc46c5448be23d83987ce
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-14 14:45:24 +0530

    remove fill function overhaul

--- src/ui/loader-preview.tsx
+++ src/ui/loader-preview.tsx
@@ -4,11 +4,36 @@ import "./loader.css";
 export default function LoaderPreview({ svgOption }: { svgOption: svgConfig }) {
   const [lightMode, setLightMode] = useState(false);
   //TODO: add line cap to config panel
+
+  //==  Remove fill  ===
+  function cleanFillFromStyleAttribute(svgString: string) {
+    return svgString.replace(/style="([^"]*)"/g, (match, styleContent) => {
+      const cleanedStyleContent = styleContent
+        .split(";")
+        .filter((style: string) => !style.trim().startsWith("fill:"))
+        .join(";");
+      return `style="${cleanedStyleContent}"`;
+    });
+  }
+  let cleanedSvgString = cleanFillFromStyleAttribute(svgOption.svgHtml);
+  cleanedSvgString = cleanedSvgString
+    .replace(/\sfill="[^"]+"/g, "")
+    .replace(/\sfill='[^']+'/g, "");
+  let cleanFill = cleanedSvgString
+    .replace(/\sfill="[^"]+"/gi, "")
+    .replace(/\sfill='[^']+'/gi, "");
+  cleanFill = cleanFill.replace(
+    /<style>([\s\S]*?)<\/style>/gi,
+    function (match, p1) {
+      const cleanedStyleContent = p1.replace(/fill:[^;}]+;?/gi, "");
+      return `<style>${cleanedStyleContent}</style>`;
+    }
+  );
+  
   //add styles dynamically
-  const finalSvgHtml = svgOption.svgHtml
-    .replace(
-      "<svg",
-      `<svg style="
+  const finalSvgHtml = cleanFill.replace(
+    "<svg",
+    `<svg style="
   
       animation-name: loader;
       animation-iteration-count: infinite;
@@ -26,8 +51,7 @@ export default function LoaderPreview({ svgOption }: { svgOption: svgConfig }) {
       display:block;
       margin:auto;
 "`
-    )
-    .replace(/\sfill=[^>\s]+/gi, ""); //remove fill
+  );
 
   return (
     <div className="m-6 mb-0">
